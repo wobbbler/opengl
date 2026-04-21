4c3f85cff610abe7d9a2adbab83ed5ed5847fd62
wobbbler <wobbbler9@gmail.com>
2026-04-21 11:06:19 +0300

    refactor: добавлены вспомогательные методы для работы с uniform в файлe Shader.h

--- include/Shader.h
+++ include/Shader.h
@@ -14,6 +14,18 @@ public:
   Shader(const char *vertexPath, const char *fragmentPath);
 
   void use() { glUseProgram(ID); }
+
+  // В документации советуют добавить вспомогательные функции, чтобы не писать
+  // каждый раз длинные glGetUniformLocation
+  void setBool(const std::string &name, bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+  }
+  void setInt(const std::string &name, int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+  }
+  void setFloat(const std::string &name, float value) const {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+  }
 };
 
 #endif
